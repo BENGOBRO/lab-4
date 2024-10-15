c35421ba1fd0cf5ae3afb89a5911d8ca296bcfea
Mikhail Starodubtsev <starodubtsev.ma03@gmail.com>
2024-10-15 19:30:12 +0300

    Добавлены тесты, база данных H2, загрузка образа и запуск контейнера

---
+++ src/test/java/ru/bengo/animaltracking/AnimalTrackingApplicationTest.java
@@ -0,0 +1,12 @@
+package ru.bengo.animaltracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalTrackingApplicationTest {
+
+    @Test
+    void contextLoads() {}
+
+}
